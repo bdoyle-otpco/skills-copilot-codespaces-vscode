53bc4c0be7fb0196984782d32b58f4fca4fdd59d
bdoyle-otpco <bdoyle@otpco.com>
2024-10-04 17:02:50 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,31 +1,6 @@
-// //create web server
-// var express = require('express');
-// var app = express();
-// var bodyParser = require('body-parser');
-// var fs = require('fs');
+// Create web server
 
-// //read data from comments.json
-// var commentData = fs.readFileSync('./comments.json');
-// var comments = JSON.parse(commentData);
-
-// //create server
-// app.use(express.static('public'));
-// app.use(bodyParser.json());
-
-// //add a new comment
-// app.post('/comments', function (req, res) {
-//     var newComment = req.body;
-//     comments.push(newComment);
-//     fs.writeFileSync('./comments.json', JSON.stringify(comments));
-//     res.json(comments);
-// });
-
-// //get all comments
-// app.get('/comments', function (req, res) {
-//     res.json(comments);
-// });
-
-// //listen to port 3000
-// app.listen(3000, function () {
-//     console.log('Server is running on port 3000');
-// });
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
